32660e253618d5814f8fafc8f9b972b74b4d19cd
Eblonko <vitalya147@outlook.com>
2016-09-23 15:47:54 +0600

    style

--- instagram-downloader.py
+++ instagram-downloader.py
@@ -3,7 +3,8 @@ import requests
 import os
 from sys import argv
 
-def download(url,local_filename):
+
+def download(url, local_filename):
     r = requests.get(url, stream=True)
     with open(os.getcwd()+"/"+local_filename, 'wb') as f:
         for chunk in r.iter_content(1024):
@@ -12,15 +13,18 @@ def download(url,local_filename):
                 f.flush()
     return local_filename
 
+
 def main():
     url = argv[1]
-    r = requests.get(url, params={'__a':1})
+    r = requests.get(url, params={'__a': 1})
     if r.headers['content-type'] != 'application/json':
         raise Exception('wrong link')
     if r.json()['media']['is_video']:
-        print('Saved as ' + download(r.json()['media']['video_url'],r.json()['media']['code'] + '.mp4') + '!')
+        print('Saved as ' + download(r.json()['media']['video_url'],
+                                     r.json()['media']['code'] + '.mp4') + '!')
     else:
-        print('Saved as ' + download(r.json()['media']['display_src'],r.json()['media']['code'] + '.jpg') + '!')
+        print('Saved as ' + download(r.json()['media']['display_src'],
+                                     r.json()['media']['code'] + '.jpg') + '!')
 
 if __name__ == '__main__':
     if len(argv) == 2:
